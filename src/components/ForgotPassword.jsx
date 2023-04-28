52d8133ea5c6a67c74bd4283ce6fae4cb225dc69
whiee1 <whiee__@hotmail.com>
2023-04-28 12:28:02 +0200

    Implemented forgat password functionality

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,26 +1,26 @@
 import { useState, useContext } from "react";
-
 import UserAuthContext from "./store/user_auth_context";
 import { Link, useNavigate } from "react-router-dom";
 
-const LogInForm = () => {
+const ForgotPassword = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [emailMessage, setEmailMessage] = useState(false);
   const [error, setError] = useState("");
-  const context = useContext(UserAuthContext);
 
-  const navigate = useNavigate();
+  const context = useContext(UserAuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    /*send the user email to Firebase and check if it exists in the Auth system */
     try {
       setError("");
-      const result = await context.logIn(email, password);
+      const result = await context.resetPassword(email);
+      setEmailMessage(true);
       if (result.success) {
-        navigate("/home");
+        setEmail("");
+        console.log("successssssssss");
       } else {
-        setError("Email or password is incorrect");
+        setError("Failed to reset password.");
       }
     } catch (error) {
       console.log(error.message);
@@ -29,13 +29,12 @@ const LogInForm = () => {
 
   return (
     <div className="formContainer">
-      <h2>Sign In</h2>
-      {error && (
-        <div className="error">
-          <h3>{error}</h3>
-        </div>
-      )}
       <form className="form" onSubmit={handleSubmit}>
+        {error && (
+          <div className="error">
+            <h3>{error}</h3>
+          </div>
+        )}
         <div className="fieldWrapper">
           <label htmlFor="emailField" />
           Email:
@@ -46,22 +45,12 @@ const LogInForm = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="fieldWrapper">
-          <label htmlFor="passwordInputField" />
-          Password:
-          <input
-            id="passwordInputField"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <Link to={"/forgot-password"}>Forgot Password?</Link>
-        </div>
 
-        <button type="submit">Sign In</button>
+        <button type="submit">Reset Password</button>
       </form>
+      <div>
+        <Link to={"/"}> Sign In</Link>
+      </div>
 
       <div>
         Don't have an account? <Link to={"/signup"}> Sign up!</Link>{" "}
@@ -70,4 +59,4 @@ const LogInForm = () => {
   );
 };
 
-export default LogInForm;
+export default ForgotPassword;
